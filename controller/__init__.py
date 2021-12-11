dfc1835a6821a41642b4174d9722c4d7473ab387
luizebaldoni <oliveira.luize@acad.ufsm.br>
2021-12-10 21:56:47 -0300

    documentando o controller

--- controller/__init__.py
+++ controller/__init__.py
@@ -46,29 +46,40 @@ def main():
     @app.route('/populate_table', methods=['POST'])
     def populate_table():
      with model.SQLite(os.path.join(db_path, db_name)) as cursor:
-        nome_tabela ='ATLETAS'
-        busca = 'SELECT nome, idade, naipe, categoria, posição FROM %s' % nome_tabela
+         
+        nome_tabela ='ATLETAS' #selecionando tabela para aparecer
+        busca = 'SELECT nome, idade, naipe, categoria, posição FROM %s' % nome_tabela #indicando colunas para puxar
         tabela = model.select_rows(cursor, busca)
-        dicionarios = []
+        dicionarios = [] #convertendo em dicionarios
+
         for linha in tabela:
-            dicionarios.append({'nome': linha[0], 'idade': linha [1], 'naipe': [2], 'categoria': [3], 'posição': linha [4]
+            dicionarios.append({'nome': linha[0], 'idade': linha [1], 'naipe': [2], 'categoria': [3], 
+            'posição': linha [4] #ordem dos itens é independete pq foi selecionado apenas alguns itens para puxar
                 })
-        response = jsonify(dicionarios)
+        response = jsonify(dicionarios) #mandando retornar os dicionarios
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
     @app.route('/select_and_populate_table', methods=['POST'])
     def select_and_populate_table():
         with model.SQLite(os.path.join(db_path, db_name)) as cursor:
-            nome_tabela = request.form['second_task_table_selector'] # puxei a tabela
-            busca = 'SELECT * FROM %s' % nome_tabela #selecionei oq qr buscar das tabelas
+
+            nome_tabela = request.form['second_task_table_selector'] # puxando a tabela
+            busca = 'SELECT * FROM %s' % nome_tabela #selecionando oq buscar das tabelas
             tabela = model.select_rows(cursor, busca)
+
             #convertendo em dicionarios
             dicionarios = []
             for linha in tabela:
-                if nome_tabela == 'ATLETAS':
+
+                #colocando uma condição pq cada tabela tem colunas diferentes sem esta condição nao identifica os itens
+                #Os id e codigos tiveram que permanecer pois, se excluidos teria que criar condições tambem para a busca
+                #linhas tem que ser em ordem do model pois se nao desconfigura e nao roda na hr de selecionar no dropdown
+                #pq foi definido que tudo das tabela viria no SELECT e nao so alguns itens
+
+                if nome_tabela == 'ATLETAS': 
                     dicionarios.append({
-                        'id_atleta': linha[0],
+                        'id_atleta': linha[0], 
                         'nome': linha[1],
                         'Idade': linha[2],
                         'id_time': linha[3],
@@ -79,6 +90,7 @@ def main():
                         'Naipe': linha[8],
                         'tipo': linha[9]
                     })
+
                 elif nome_tabela == 'TECNICOS':
                     dicionarios.append({
                        'id_tecnico': linha[0],
@@ -89,6 +101,7 @@ def main():
                         'ja_foi_atleta': linha[5],
                         'treina_o_naipe': linha[6]
                 })
+
                 elif nome_tabela == 'TIMES':
                     dicionarios.append({
                         'id_time': linha[0],
@@ -98,19 +111,21 @@ def main():
                         'país': linha[4],
                         'tipo': linha[5],
                         'Competição_atual':linha[6]
-
                     })
+
                 elif nome_tabela == 'ATLETA_para_TIME':
                     dicionarios.append({
                         'id_atleta': linha[0],
                         'id_time':linha[1]
                     })
+
                 elif nome_tabela == 'TECNICO_para_TIME':
                    dicionarios.append({
                         'id_tecnico': linha[0],
                         'id_time':linha[1]
                    }) 
-            response = jsonify(dicionarios)
+
+            response = jsonify(dicionarios) #mandando retornar os dicionarios
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
 
